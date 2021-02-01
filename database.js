5cb8abbfe42e8014788d392201e5ece1620aa98a
Alex Miller <fotoflo@gmail.com>
2021-02-02 07:55:33 +0800

    lint

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 // dependencies
-const mysql = require('mysql');
+const mysql = require('mysql')
 const createTables = require('./createTables')
 const {printDBsettings} = require('commonutils')
 
@@ -21,63 +21,63 @@ printDBsettings(settings)
 
 if(!settings.database){
   console.log("\nNo Database credentials in Envirnment, please edit .env and run source .env")
-  process.exit();
+  process.exit()
 }
 
-const client = mysql.createConnection(settings);  
+const client = mysql.createConnection(settings)
 
 
 function connect (callback) {
   client.connect(function(err) {
   if (err) {
     console.log(settings)
-    console.error('error connecting to db: ', err);
-    return;
+    console.error('error connecting to db: ', err)
+    process.exit()
   }
 
-  console.log('connected to db as id ' + client.threadId);
-  });
+  console.log('connected to db as id ' + client.threadId)
+  })
 
   console.log("creating tables")
 
   createTables(client)
 
   // Call the callback
-  callback();
+  callback()
 }
 
 // Disconnect from the database
 function disconnect () {
   client.end(err => {
-    console.log('Disconnected from database');
+    console.log('Disconnected from database')
 
     if (err) {
-      console.log('There was an error during disconnection', err.stack);
+      console.log('There was an error during disconnection', err.stack)
     }
-  });
+  })
 }
 
 async function query (query_text, query_params) {
 
   if(query_params && query_params.fetch_time){
-    query_params.fetch_time = query_params.fetch_time.replace(/Z/, '');
-    query_params.fetch_time = query_params.fetch_time.replace(/T/, ' ');
+    query_params.fetch_time = query_params.fetch_time.replace(/Z/, '')
+    query_params.fetch_time = query_params.fetch_time.replace(/T/, ' ')
   }
 
   if(query_params && query_params.report && query_params.report.fetchTime){
-    query_params.report.fetchTime = query_params.report.fetchTime.replace(/Z/, '');
-    query_params.report.fetchTime = query_params.report.fetchTime.replace(/T/, ' ');
+    query_params.report.fetchTime = query_params.report.fetchTime.replace(/Z/, '')
+    query_params.report.fetchTime = query_params.report.fetchTime.replace(/T/, ' ')
   }
 
   try {
-    const result = await client.query(query_text, query_params);
+    const result = await client.query(query_text, query_params)
     // console.log(`query ${query_text} returned ${result}`)
-    return result;
+    return result
   }catch (err) {
     console.log("********* CAUGHT ERROR *********")
     console.log("query_text", query_text)
     console.log("\nquery_params", query_params)
-    console.error(err);
+    console.error(err)
   }
 }
 
@@ -85,4 +85,4 @@ module.exports = {
   connect,
   disconnect,
   query
-};
+}
